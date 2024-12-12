b48eb492db5bc17e8d3e8312f264c82846eae023
Deepesh <sainideepesh12@gmail.com>
2024-12-12 17:36:56 +0530

    add questions

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import QuestionData from "./questions.js";
+
 const createUser = document.querySelector("#btn1");
 const startQuiz = document.querySelector("#btn2");
 const Create = document.querySelector("#btn-btn1");
@@ -10,6 +12,14 @@ const main = document.querySelector("#main");
 const red = document.querySelector("#red p");
 const green = document.querySelector("#green p");
 const heading4 = document.querySelector("#heading4");
+let dummy = [];
+let question = null;
+const questiondiv = document.querySelector(".question");
+const options = document.querySelectorAll(".option");
+const mainOption = document.querySelector("#options");
+const quizdiv = document.querySelector("#quiz");
+const startdivbutton = document.querySelector("#sub-start");
+const selectboxes = document.querySelector("#content-starts");
 
 
 
@@ -38,9 +48,7 @@ function create() {
         parent.append(div);
         createUser.style.display = "none"
         localStorage.setItem("ls_name", input.value);
-
         green.style.display = "block";
-
         setTimeout(() => {
             green.style.display = "none";
         }, 2000);
@@ -70,8 +78,34 @@ function START() {
     }
 
 
+}
 
 
+startdivbutton.addEventListener("click", displayquestion);
+
+
+function displayquestion() {
+    quizdiv.classList.remove("gayab");
+    selectboxes.classList.add("gayab");
+    showquestionoptions();
+}
+function showquestionoptions() {
+    questiondiv.innerHTML = QuestionData[selectquestion()].q;
+    options.forEach((option, index) => {
+        option.innerHTML = QuestionData[question].opt[index];
+    });
+    // mainOption.classList.remove("gayab")
+}
+
+function selectquestion() {
+    question = Math.floor(Math.random() * QuestionData.length);
+
+    if (dummy.includes(question)) return selectquestion()
+    else {
+        console.log(question)
+        dummy.push(question)
+        return question
+    }
 }
 
 
