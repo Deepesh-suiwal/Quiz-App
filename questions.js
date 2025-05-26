e48978727be71cba6f654d991a174d0bfb6151d6
Deepesh <sainideepesh12@gmail.com>
2025-05-26 23:23:07 +0530

    improve code

--- questions.js
+++ questions.js
@@ -1,29 +1,93 @@
 const QuestionData = [
-    {
-        q: "Q:Who is the father of C language?",
-        a: "Dennis Ritchie",
-        opt: ["Steve Jobs", "James Gosling", "Dennis Ritchie", "Rasmus Lerdorf"],
-    },
-    {
-        q: "Q:Which of the following is not a valid C variable name?",
-        a: "int $main",
-        opt: ["int number", "int float", "int $main", "float char"],
-    },
-    {
-        q: "Q:All keywords in C are in ____________",
-        a: "LowerCase letters",
-        opt: ["LowerCase letters", "UpperCase letters", "CamelCase letters", "None of the mentioned"],
-    },
-    {
-        q: "Q:What is the result of logical or relational expression in C?",
-        a: "0 or 1",
-        opt: ["0 or 1", "True or False", "1 or 0", "None of the mentioned"],
-    },
-    {
-        q: "Q:Which of these is not a programming language?",
-        a: "Quick",
-        opt: ["Quick", "C", "C++", "Python"],
-    },
-
+  {
+    q: "Q:Who is the father of C language?",
+    a: "Dennis Ritchie",
+    opt: ["Steve Jobs", "James Gosling", "Dennis Ritchie", "Rasmus Lerdorf"],
+  },
+  {
+    q: "Q:Which of the following is not a valid C variable name?",
+    a: "int $main",
+    opt: ["int number", "int float", "int $main", "float char"],
+  },
+  {
+    q: "Q:All keywords in C are in ____________",
+    a: "LowerCase letters",
+    opt: [
+      "LowerCase letters",
+      "UpperCase letters",
+      "CamelCase letters",
+      "None of the mentioned",
+    ],
+  },
+  {
+    q: "Q:What is the result of logical or relational expression in C?",
+    a: "0 or 1",
+    opt: ["0 or 1", "True or False", "1 or 0", "None of the mentioned"],
+  },
+  {
+    q: "Q:Which of these is not a programming language?",
+    a: "Quick",
+    opt: ["Quick", "C", "C++", "Python"],
+  },
 ];
-export default QuestionData;
+const MusicQuestionData = [
+  {
+    q: "Q:Who is known as the 'King of Pop'?",
+    a: "Michael Jackson",
+    opt: ["Elvis Presley", "Freddie Mercury", "Michael Jackson", "Prince"],
+  },
+  {
+    q: "Q:Which instrument has keys, pedals, and strings?",
+    a: "Piano",
+    opt: ["Guitar", "Violin", "Piano", "Drums"],
+  },
+  {
+    q: "Q:Which music genre originated in the African-American communities of New Orleans?",
+    a: "Jazz",
+    opt: ["Rock", "Jazz", "Hip Hop", "Blues"],
+  },
+  {
+    q: "Q:Who composed the famous piece 'Fur Elise'?",
+    a: "Ludwig van Beethoven",
+    opt: ["Mozart", "Bach", "Ludwig van Beethoven", "Chopin"],
+  },
+  {
+    q: "Q:Which of these is not a musical term?",
+    a: "Compile",
+    opt: ["Allegro", "Tempo", "Compile", "Harmony"],
+  },
+];
+const ModernArtQuestionData = [
+  {
+    q: "Q:Who is considered the pioneer of Cubism?",
+    a: "Pablo Picasso",
+    opt: ["Vincent van Gogh", "Pablo Picasso", "Claude Monet", "Andy Warhol"],
+  },
+  {
+    q: "Q:Which art movement is associated with abstract shapes and primary colors?",
+    a: "De Stijl",
+    opt: ["Surrealism", "Impressionism", "De Stijl", "Pop Art"],
+  },
+  {
+    q: "Q:What is the style of painting used by Jackson Pollock known as?",
+    a: "Action Painting",
+    opt: ["Pointillism", "Action Painting", "Realism", "Minimalism"],
+  },
+  {
+    q: "Q:Which modern artist is famous for painting Campbell’s soup cans?",
+    a: "Andy Warhol",
+    opt: [
+      "Roy Lichtenstein",
+      "Andy Warhol",
+      "Piet Mondrian",
+      "Jean-Michel Basquiat",
+    ],
+  },
+  {
+    q: "Q:Which of these is not a modern art movement?",
+    a: "Baroque",
+    opt: ["Baroque", "Dadaism", "Futurism", "Expressionism"],
+  },
+];
+
+export { QuestionData, MusicQuestionData, ModernArtQuestionData };
--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-import QuestionData from "./questions.js";
+import {
+  QuestionData,
+  MusicQuestionData,
+  ModernArtQuestionData,
+} from "./questions.js";
 
 const createUser = document.querySelector("#btn1");
 const startQuiz = document.querySelector("#btn2");
@@ -15,6 +19,9 @@ const heading4 = document.querySelector("#heading4");
 const timer = document.querySelector(".timer");
 const wellDoneBox = document.querySelector("#welldone");
 const coding = document.querySelector(".topicThree");
+const music = document.querySelector(".topicOne");
+const modernArt = document.querySelector(".topicTwo");
+
 const stop = document.querySelector("#Stop");
 const showresult = document.querySelector(".showresult");
 const getresult = document.querySelector("#Result");
@@ -27,269 +34,256 @@ const tickboxes = document.querySelector("#content-starts");
 const scorebox = document.querySelector("#scorebox");
 const displayscore = document.querySelector("#displayfinalscore");
 const displaydate = document.querySelector("#displaydateandtime");
-
-
-
-
-
-
+const category = document.querySelector("#category");
 let normal = 0;
 let normal1 = 0;
-let time = 5
+let time = 5;
 let dummy = [];
+let selectedCategory = "";
 let useranswer = [];
 let user = null;
 let question = null;
+let QuestionType = null;
 const questiondiv = document.querySelector(".question");
 const options = document.querySelectorAll(".option");
 const mainOption = document.querySelector("#options");
 const quizdiv = document.querySelector("#quiz");
 const startdivbutton = document.querySelector("#sub-start");
 const selectboxes = document.querySelector("#main");
-const playerNames = localStorage.getItem("ls_name") === null ? [] : JSON.parse(localStorage.getItem("ls_name"));
+const playerNames =
+  localStorage.getItem("ls_name") === null
+    ? []
+    : JSON.parse(localStorage.getItem("ls_name"));
 let interval;
 
-
-
-
 Quit.addEventListener("click", () => {
-    popup1.style.display = "none";
-    input.value = "";
-})
+  popup1.style.display = "none";
+  input.value = "";
+});
 
 createUser.addEventListener("click", () => {
-    popup1.style.display = "block";
-})
+  popup1.style.display = "block";
+});
 
 Create.addEventListener("click", create);
 
 function create() {
-    if (input.value === "") {
-        alert("Please enter your name:");
-    }
-    else {
-        popup1.style.display = "none";
-        createUser.style.display = "none"
-        const obj = { name: input.value };
-        playerNames.push(obj);
-        localStorage.setItem("ls_name", JSON.stringify(playerNames));
-        let div = document.createElement("div");
-        div.classList = "user";
-        user = JSON.parse(localStorage.getItem("ls_name"));
-        div.innerHTML = user[user.length - 1].name
-        div.style.color = "#000";
-        parent.append(div);
-        green.style.display = "block";
-        setTimeout(() => {
-            green.style.display = "none";
-        }, 2000);
-        name()
-
-
-    }
-
+  if (input.value === "") {
+    alert("Please enter your name:");
+  } else {
+    popup1.style.display = "none";
+    createUser.style.display = "none";
+    const obj = { name: input.value };
+    playerNames.push(obj);
+    localStorage.setItem("ls_name", JSON.stringify(playerNames));
+    let div = document.createElement("div");
+    div.classList = "user";
+    user = JSON.parse(localStorage.getItem("ls_name"));
+    div.innerHTML = user[user.length - 1].name;
+    div.style.color = "#000";
+    parent.append(div);
+    green.style.display = "block";
+    setTimeout(() => {
+      green.style.display = "none";
+    }, 2000);
+    name();
+  }
 }
 
 startQuiz.addEventListener("click", START);
 
 function START() {
+  if (input.value === "") {
+    red.style.display = "block";
 
-    if (input.value === "") {
-        red.style.display = "block";
-
-        setTimeout(() => {
-            red.style.display = "none";
-        }, 2000);
-    }
-
-    else {
-        wrapper.style.display = "none";
-        selectboxes.classList.remove("gayab");
-        heading4.append(user[user.length - 1].name)
-    }
-
-
+    setTimeout(() => {
+      red.style.display = "none";
+    }, 2000);
+  } else {
+    wrapper.style.display = "none";
+    selectboxes.classList.remove("gayab");
+    heading4.append(user[user.length - 1].name);
+  }
 }
-
 
 startdivbutton.addEventListener("click", displayquestion);
 
-
 function displayquestion() {
-    if (normal <= 0) {
-        alert("Please select one option out of 3 options");
-    }
-    else {
-        quizdiv.classList.remove("gayab");
-        selectboxes.classList.add("gayab");
-        normal1++;
-        startTime()
-        showquestionoptions();
-    }
-
+  if (normal <= 0) {
+    alert("Please select one option out of 3 options");
+  } else {
+    quizdiv.classList.remove("gayab");
+    selectboxes.classList.add("gayab");
+    normal1++;
+    startTime();
+    showquestionoptions();
+  }
 }
 function showquestionoptions() {
-    questiondiv.innerHTML = QuestionData[selectquestion()].q;
-    options.forEach((option, index) => {
-        option.innerHTML = QuestionData[question].opt[index];
-    });
-    mainOption.classList.remove("gayab")
+  questiondiv.innerHTML = QuestionType[selectquestion()].q;
+  options.forEach((option, index) => {
+    option.innerHTML = QuestionType[question].opt[index];
+  });
+  mainOption.classList.remove("gayab");
 }
 
 function selectquestion() {
-    question = Math.floor(Math.random() * QuestionData.length);
+  question = Math.floor(Math.random() * QuestionType.length);
 
-    if (dummy.includes(question)) return selectquestion()
-    else {
-        // console.log(question)
-        dummy.push(question)
-        return question
-    }
+  if (dummy.includes(question)) return selectquestion();
+  else {
+    // console.log(question)
+    dummy.push(question);
+    return question;
+  }
 }
 
 function startTime() {
-    timer.innerHTML = time
-    interval = setInterval(() => {
-        if (time == 1) {
-            time = 5;
-            timer.innerHTML = time
+  timer.innerHTML = time;
+  interval = setInterval(() => {
+    if (time == 1) {
+      time = 5;
+      timer.innerHTML = time;
 
-            if (dummy.length == 5) {
-                quizdiv.classList.add("gayab");
-                wellDoneBox.classList.remove("gayab");
-                clearInterval(interval)
-            }
-            else {
-                options.forEach(option => {
-                    option.classList.remove("clicked")
-                })
-                showquestionoptions()
-                mainOption.classList.remove("gayab");
-
-            }
-        }
-        else {
-            timer.innerHTML = --time
-        }
-    }, 1000)
+      if (dummy.length == 5) {
+        quizdiv.classList.add("gayab");
+        wellDoneBox.classList.remove("gayab");
+        clearInterval(interval);
+      } else {
+        options.forEach((option) => {
+          option.classList.remove("clicked");
+        });
+        showquestionoptions();
+        mainOption.classList.remove("gayab");
+      }
+    } else {
+      timer.innerHTML = --time;
+    }
+  }, 1000);
 }
 
+coding.addEventListener("click", () => bordergreen(coding, QuestionData));
+music.addEventListener("click", () => bordergreen(music, MusicQuestionData));
+modernArt.addEventListener("click", () =>
+  bordergreen(modernArt, ModernArtQuestionData)
+);
 
-coding.addEventListener("click", bordergreen);
+function bordergreen(element, type) {
+  QuestionType = type;
 
-function bordergreen() {
-    coding.style.border = "4px solid green"
-    icon.style.display = "block"
-    normal++;
+  if (element === coding) selectedCategory = "Coding";
+  else if (element === music) selectedCategory = "Music";
+  else if (element === modernArt) selectedCategory = "Modern Art";
+
+  // Highlight selected
+  [coding, music, modernArt].forEach((el) => {
+    el.style.border = "1px solid #ccc"; // Reset border
+  });
+  element.style.border = "4px solid green";
+
+  normal = 1;
 }
 
 stop.addEventListener("click", stopquiz);
 
 function stopquiz() {
-    selectboxes.classList.remove("gayab");
-    quizdiv.classList.add("gayab");
-    icon.style.display = "none";
-    normal = 0;
-    dummy = []
-    clearInterval(interval)
+  selectboxes.classList.remove("gayab");
+  quizdiv.classList.add("gayab");
+  icon.style.display = "none";
+  normal = 0;
+  dummy = [];
+  clearInterval(interval);
 }
 Quit2.addEventListener("click", quitsession);
 
 function quitsession() {
-    selectboxes.classList.remove("gayab");
-    wellDoneBox.style.display = "none";
-    icon.style.display = "none";
-    // normal = 0;
-
+  selectedCategory = "";
+  question = null;
+  dummy = [];
+  selectboxes.classList.remove("gayab");
+  wellDoneBox.style.display = "none";
+  icon.style.display = "none";
+  // normal = 0;
 }
 
-
 options.forEach((option) => {
-    option.addEventListener("click", storeUserAnswer);
-
+  option.addEventListener("click", storeUserAnswer);
 });
 
 function storeUserAnswer(e) {
-    useranswer.push(e.target.innerHTML);
-    options.forEach(option => {
-        option.classList.add("clicked")
-    })
+  useranswer.push(e.target.innerHTML);
+  options.forEach((option) => {
+    option.classList.add("clicked");
+  });
 }
 
 function calculateScore() {
-    console.log(useranswer);
-    console.log(dummy)
-    let finalScore = 0;
-    useranswer.forEach((answer, index) => {
-        if (answer == QuestionData[dummy[index]].a) {
-            console.log(answer, QuestionData[dummy[index]].a);
-            finalScore++;
-        }
-
-    });
-    showresult.innerHTML =
-        "Your score is " + finalScore + " out of " + QuestionData.length;
-    displayscore.innerHTML =
-        finalScore + " out of " + QuestionData.length;
+  console.log(useranswer);
+  console.log(dummy);
+  let finalScore = 0;
+  useranswer.forEach((answer, index) => {
+    if (answer == QuestionType[dummy[index]].a) {
+      console.log(answer, QuestionType[dummy[index]].a);
+      finalScore++;
+    }
+  });
+  showresult.innerHTML =
+    "Your score is " + finalScore + " out of " + QuestionType.length;
+  category.innerHTML = selectedCategory;
+  displayscore.innerHTML = finalScore + " out of " + QuestionType.length;
 }
 
 getresult.addEventListener("click", displayresult);
 
 function displayresult() {
-    showresult.style.display = "block"
-    calculateScore()
+  showresult.style.display = "block";
+  calculateScore();
 }
 
 nextquestion.addEventListener("click", nextquestiondisplay);
 
 function nextquestiondisplay() {
-    options.forEach(option => {
-        option.classList.remove("clicked")
-    })
-    showquestionoptions()
-    time = 5;
-    timer.innerHTML = time
+  options.forEach((option) => {
+    option.classList.remove("clicked");
+  });
+  showquestionoptions();
+  time = 5;
+  timer.innerHTML = time;
 }
 
 playagain.addEventListener("click", startagain);
 
 function startagain() {
-    wellDoneBox.classList.add("gayab");
-    dummy = [];
-    showresult.style.display = "none";
-    useranswer.length = 0;
-    displayquestion()
+  wellDoneBox.classList.add("gayab");
+  dummy = [];
+  showresult.style.display = "none";
+  useranswer.length = 0;
+  displayquestion();
 }
 
 clearscreen.addEventListener("click", screen1);
 
 function screen1() {
-    selectboxes.style.display = "none";
-    wrapper.style.display = "block";
+  selectboxes.style.display = "none";
+  wrapper.style.display = "block";
 }
 
-
-ShowScore.addEventListener("click", ShowScore1)
-
+ShowScore.addEventListener("click", ShowScore1);
 
 function ShowScore1() {
-    if (normal1 <= 0) {
-        alert("first start a quiz")
-    }
-    else {
-        tickboxes.classList.add("gayab");
-        scorebox.style.display = "block";
-    }
-
-
-
+  if (normal1 <= 0) {
+    alert("first start a quiz");
+  } else {
+    tickboxes.classList.add("gayab");
+    scorebox.style.display = "block";
+  }
 }
 
 function name() {
-    const date = new Date();
-    const time = date.toLocaleString();
-    localStorage.setItem("TD", time);
-    let data = localStorage.getItem("TD");
+  const date = new Date();
+  const time = date.toLocaleString();
+  localStorage.setItem("TD", time);
+  let data = localStorage.getItem("TD");
 
-    displaydate.innerHTML = data
+  displaydate.innerHTML = data;
 }
